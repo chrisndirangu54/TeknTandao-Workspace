e7cb8a199cba4cbb2cac3c7636c375def5b246e4
chrisndirangu54 <48279433+chrisndirangu54@users.noreply.github.com>
2026-09-14 09:33:22 +0300

    Expose advanced website platform functions

--- functions/src/platform_entry.js
+++ functions/src/platform_entry.js
@@ -3,3 +3,4 @@ export * from './sota_core.js';
 export * from './sota_extensions.js';
 export * from './graph_projection.js';
 export * from './website_builder.js';
+export * from './website_builder_advanced.js';
