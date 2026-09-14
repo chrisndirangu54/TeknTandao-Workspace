0a2437aaecb3c87036e455aa873ac7c1aa7a9f6d
chrisndirangu54 <48279433+chrisndirangu54@users.noreply.github.com>
2026-09-14 12:57:47 +0300

    Expose Firebase FinOps controls

--- functions/src/platform_entry.js
+++ functions/src/platform_entry.js
@@ -2,6 +2,7 @@ export * from './index.js';
 export * from './sota_core.js';
 export * from './sota_extensions.js';
 export * from './graph_projection.js';
+export * from './firebase_finops.js';
 export {
   createWebsiteProject,
   replaceWebsiteDocument,
