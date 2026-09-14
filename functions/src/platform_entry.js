9750e39a9ec153be087211c46547b16626fee9c3
chrisndirangu54 <48279433+chrisndirangu54@users.noreply.github.com>
2026-09-14 10:46:18 +0300

    Export website business AI integration

--- functions/src/platform_entry.js
+++ functions/src/platform_entry.js
@@ -24,3 +24,5 @@ export {patchWebsiteProject} from './website_builder_crdt_patch.js';
 export {exportAdvancedWebsiteFlutterScaffold as exportWebsiteFlutterScaffold} from './website_builder_scaffold.js';
 export * from './website_builder_advanced.js';
 export * from './website_builder_payout_webhook.js';
+export * from './website_business_ai.js';
+export * from './website_business_agent_governance.js';
