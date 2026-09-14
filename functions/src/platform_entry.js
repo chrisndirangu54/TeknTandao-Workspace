dc946094a131787882703770ce44d83180244dae
chrisndirangu54 <48279433+chrisndirangu54@users.noreply.github.com>
2026-09-14 09:41:12 +0300

    Make visual website patch API CRDT-backed

--- functions/src/platform_entry.js
+++ functions/src/platform_entry.js
@@ -2,5 +2,24 @@ export * from './index.js';
 export * from './sota_core.js';
 export * from './sota_extensions.js';
 export * from './graph_projection.js';
-export * from './website_builder.js';
+export {
+  createWebsiteProject,
+  replaceWebsiteDocument,
+  publishWebsiteProject,
+  rollbackWebsiteProject,
+  getPublishedWebsite,
+  exportWebsiteFlutterScaffold,
+  publishWebsiteTemplate,
+  unpublishWebsiteTemplate,
+  getWebsiteTemplateMarketplace,
+  getWebsiteTemplatePreview,
+  installWebsiteTemplate,
+  startWebsiteTemplatePurchase,
+  checkWebsiteTemplateMpesaPurchase,
+  websiteTemplatePaystackWebhook,
+  getWebsiteCreatorDashboard,
+  generateWebsiteFromPrompt,
+  getWebsiteBuilderOverview
+} from './website_builder.js';
+export {patchWebsiteProject} from './website_builder_crdt_patch.js';
 export * from './website_builder_advanced.js';
